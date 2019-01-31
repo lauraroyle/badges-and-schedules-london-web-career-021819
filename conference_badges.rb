41c7a0b87387c56a62b11b5d3f1529b05fae7846
Laura Royle <lauraamie.royle@gmail.com>
2019-01-31 11:54:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ end
 def assign_rooms(attendees)
   room = 0
   attendees.collect do |name|
-    "Hello #{name}! You'll be assigned to room #{room}!"
+    puts "Hello #{name}! You'll be assigned to room #{room}!"
     room += 1
   end
 end
